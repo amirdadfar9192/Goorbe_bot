f46ecf4465d33317c0c1aa93e878bc33ec3ffe53
amirdadfar9192 <65132126+amirdadfar9192@users.noreply.github.com>
2022-01-23 09:13:08 +0330

    i may have to go out to get a fucking life but i dont think i will

--- cogs/Socials.py
+++ cogs/Socials.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
 import time 
-import ffmpeg
+
 from discord import FFmpegPCMAudio, channel
-import DiscordUtilsMod
-music = DiscordUtilsMod.Music()
+
 
 class Socials(commands.Cog):
 
@@ -14,16 +13,6 @@ class Socials(commands.Cog):
     @commands.command()
     async def what(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/821739231096602689/917020202871435275/unknown.png")
-    @commands.command()
-    async def jumpscare(self, ctx):
-        if (ctx.author.voice):
-            channel = ctx.message.author.voice.channel
-            voice = await channel.connect()
-            source = FFmpegPCMAudio('jumpscare.ogg')
-            player = channel.play(source)
-            time.sleep(8)
-            await ctx.voice_client.disconnect()    
-
     @commands.command()
     async def info(ctx):
         await ctx.send("`Commands = play to play a song(p,P,Play,play), join to join voice,remove to remove a song from your queue, skip to skip a song,np to see what song is playing right now, join to join your voice channel , resume to resume a paused song, pause to pause a song , leave to leave a vc,stop to stop the whole queue.Kick/Ban @user to kick/ban a user.Use % as its the bot's prefix . *This Bot Is just For Fun But I'll keep Updating This Project* , Enjoy <3 `")
