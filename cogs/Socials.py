ee638c356ef815f4333d59e9a3f2062debb996fa
Amir <amirdadfar9192@gmail.com>
2022-02-01 11:30:53 +0330

    Update Socials.py

--- cogs/Socials.py
+++ cogs/Socials.py
@@ -84,7 +84,7 @@ class Socials(commands.Cog):
     
 #events
     @commands.Cog.listener()
-    async def on_member_join(member : discord.Member,*, message=None):
+    async def on_member_join(member : discord.Member,message=None):
         chmessage = "Welcome To Our Server :) "
         embed = discord.Embed(title = chmessage)
         await member.send(embed=embed)
