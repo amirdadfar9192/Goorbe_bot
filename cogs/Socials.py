6c61a799308aceb53d202225636a7a944429c603
Amir <amirdadfar9192@gmail.com>
2022-02-16 23:28:59 +0330

    clear func

--- cogs/Socials.py
+++ cogs/Socials.py
@@ -16,6 +16,32 @@ class Socials(commands.Cog):
     @commands.command()
     async def what(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/821739231096602689/917020202871435275/unknown.png")
+
+    @commands.command(name="end_cord", aliases=["endportal", "portal_cord"])
+    async def end_cord(self, ctx):
+        embed = discord.Embed(title="Cords:", description='X:86 Y:58 Z:-3', color=0xe9dc43)
+        embed.set_thumbnail(
+            url='https://my.mcpedl.com/storage/texturepacks/775/images/nice-cats-texture-pack_3.png')
+        embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="home", aliases=["minecraft_home"])
+    async def home(self, ctx):
+        embed = discord.Embed(title="Cords:", description="X:-359 Y:121 Z:-61", color=0xe9dc43)
+        embed.set_thumbnail(
+            url='https://my.mcpedl.com/storage/texturepacks/775/images/nice-cats-texture-pack_3.png')
+        embed.set_footer(text=f"requested by {ctx.author.name}"
+                         , icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="stronghold", aliases=["Stronghold", "str_cord", "Str_cord"])
+    async def stronghold(self, ctx):
+        embed = discord.Embed(title="Cords:", description="X:-696 Y:~ Z:1432", color=0xe9dc43)
+        embed.set_thumbnail(
+            url='https://my.mcpedl.com/storage/texturepacks/775/images/nice-cats-texture-pack_3.png')
+        embed.set_footer(text=f'Reqeusted by {ctx.author.name}', icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
     @commands.command()
     async def info(ctx):
         await ctx.send("`Commands = play to play a song(p,P,Play,play), join to join voice,remove to remove a song from your queue, skip to skip a song,np to see what song is playing right now, join to join your voice channel , resume to resume a paused song, pause to pause a song , leave to leave a vc,stop to stop the whole queue.Kick/Ban @user to kick/ban a user.Use % as its the bot's prefix . *This Bot Is just For Fun But I'll keep Updating This Project* , Enjoy <3 `")
