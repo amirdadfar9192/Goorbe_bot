4eec7ac7f52f4360b4166407793cbde2bc2b7ba9
amiratkali <amirdadfar9192@gmail.com>
2022-02-27 15:49:22 -0500

    on_command_error EVENT ADDED

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -16,7 +16,8 @@ class Admin(commands.Cog):
         embed = discord.Embed(title="Amir", url="https://github.com/amirdadfar9192",
                               description="`Commands = play to play a song(p,P,Play,play), join to join voice,remove to remove a song from your queue, skip to skip a song,np to see what song is playing right now, join to join your voice channel , resume to resume a paused song, pause to pause a song , leave to leave a vc,stop to stop the whole queue.Kick/Ban @user to kick/ban a user.Use % as its the bot's prefix . *This Bot Is just For Fun But I'll keep Updating This Project* , Enjoy <3 `",
                               color=0x8000ff)
-        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        embed.set_author(name=ctx.author.display_name,
+                         icon_url=ctx.author.avatar_url)
         embed.set_thumbnail(
             url='https://cdn.discordapp.com/avatars/365898254631567360/d325980732e089e58664f01c7174bac9.webp')
         embed.set_footer(text="Thanks For Using This Bot :)")
@@ -81,7 +82,8 @@ class Admin(commands.Cog):
             em = discord.Embed(
                 title="React to this message with :no_entry_sign: to confirm clearing the whole text channel. (This message automatically deletes after 5 seconds)",
                 colour=discord.Color.purple())
-            em.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+            em.set_footer(
+                text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
 
             confirmMsg = await ctx.send(embed=em)
             await confirmMsg.add_reaction('\N{NO ENTRY SIGN}')
@@ -92,8 +94,10 @@ class Admin(commands.Cog):
         if isinstance(amount, int) and amount > 0:
             await ctx.channel.purge(limit=amount + 1)
 
-            em = discord.Embed(title=f"**{amount}** messages were deleted", colour=discord.Color.purple())
-            em.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+            em = discord.Embed(
+                title=f"**{amount}** messages were deleted", colour=discord.Color.purple())
+            em.set_footer(
+                text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
 
             delMsg = await ctx.send(embed=em)
             await asyncio.sleep(3)
@@ -101,9 +105,14 @@ class Admin(commands.Cog):
         else:
             em = discord.Embed(title=f"Please specify a valid number of messages to delete",
                                colour=discord.Color.purple())
-            em.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+            em.set_footer(
+                text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
 
             return await ctx.send(embed=em)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await ctx.send("There Was An Error With This Command.")
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -114,8 +123,10 @@ class Admin(commands.Cog):
         if reaction.message.id == confirmMsgID and reaction.emoji == '\N{NO ENTRY SIGN}' and user.id != self.bot.user.id and user.id == confirmMsgUserID:
             await reaction.message.channel.purge(limit=None)
 
-            em = discord.Embed(title=f"This channel was cleared", colour=discord.Color.purple())
-            em.set_footer(text=f"Requested by {user.name}", icon_url=user.avatar_url)
+            em = discord.Embed(title=f"This channel was cleared",
+                               colour=discord.Color.purple())
+            em.set_footer(
+                text=f"Requested by {user.name}", icon_url=user.avatar_url)
 
             delMsg = await reaction.message.channel.send(embed=em)
             await asyncio.sleep(3)
